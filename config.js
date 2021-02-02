237c99c6943ec4043272f9c342566200b7e7c633
xi-ui <74759591+xi-ui@users.noreply.github.com>
2021-02-02 12:28:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ module.exports = {
     rss: '/rss.xml',
   },
   contactFormUrl: process.env.CONTACT_FORM_ENDPOINT || 'https://getform.io/f/09a3066f-c638-40db-ad59-05e4ed71e451',
-  googleAnalyticTrackingId: process.env.GA_TRACKING_ID || '',
+  googleAnalyticTrackingId: process.env.GA_TRACKING_ID || 'G-GF0G6W5PKB',
   tags: {
     javascript: {
       name: 'javascript',
